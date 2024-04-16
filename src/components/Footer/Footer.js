54138a7bfcd3e8165f7b8663c1586b207055bf78
Chaitu9059 <chaitanya.patalam@gmail.com>
2024-04-16 15:17:59 +0530

    first

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import logo from "../../assets/circles.png";
+
+const Footer = () => {
+  return (
+    <MDBFooter color="unique-color-dark" className="font-medium pt-4 mt-4">
+      <MDBContainer className="text-center text-md-left">
+        <MDBRow className="text-center text-md-left mt-3 pb-3">
+          <MDBCol md="3" lg="3" xl="4" className="mx-auto mt-3">
+            <h6 className="text-uppercase mb-4 font-weight-bold">
+              <img src={logo} alt="Book Store App" height="50px" />
+              <strong>Book-IT</strong>
+            </h6>
+            <p>
+            Book-IT is a React-based online bookstore where customers can easily buy books.
+            Within this platform, users have the ability to search for books by title,
+            add them to their shopping cart, and ultimately complete their purchases via
+            secure credit card transactions.
+            </p>
+          </MDBCol>
+          <hr className="w-100 clearfix d-md-none" />
+          <MDBCol md="2" lg="2" xl="2" className="mx-auto mt-3">
+            <h6 className="text-uppercase mb-4 font-weight-bold">
+              <strong>Products</strong>
+            </h6>
+            <p>
+              <a href="#">Book-IT</a>
+            </p>
+            <p>
+              <a href="https://github.com/Chaitu9059">Portfolio</a>
+            </p>
+          </MDBCol>
+
+          <hr className="w-100 clearfix d-md-none" />
+          <MDBCol md="4" lg="3" xl="3" className="mx-auto mt-3">
+            <h6 className="text-uppercase mb-4 font-weight-bold">
+              <strong>Contact</strong>
+            </h6>
+            <p>
+              <i className="fa fa-envelope mr-3" /> &nbsp;chaitanya.patalam@gmail.com
+            </p>
+            <p>
+              <a
+                className="btn-floating btn-sm rgba-white-slight mx-1"
+                href="https://www.linkedin.com/in/chaitanya-kumar-patalam/"
+              >
+                <i className="fab fa-linkedin-in" /> &nbsp;LinkedIn
+              </a>
+            </p>
+          </MDBCol>
+        </MDBRow>
+        <hr />
+        <MDBRow className="d-flex align-items-center">
+          <MDBCol md="8" lg="8">
+            <p className="text-center text-md-left grey-text">
+              &copy; {new Date().getFullYear()} Made by
+              <a href=""> Chaitanya Kumar </a>
+            </p>
+          </MDBCol>
+          <MDBCol md="4" lg="4" className="ml-lg-0">
+            <div className="text-center text-md-right">
+              <ul className="list-unstyled list-inline">
+                <li className="list-inline-item">
+                  <a
+                    className="btn-floating btn-sm rgba-white-slight mx-1"
+                    href="https://github.com/Chaitu9059"
+                  >
+                    <i className="fab fa-github" />
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a
+                    className="btn-floating btn-sm rgba-white-slight mx-1"
+                    href="https://www.linkedin.com/in/chaitanya-kumar-patalam/"
+                  >
+                    <i className="fab fa-linkedin-in" />
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a
+                    className="btn-floating btn-sm rgba-white-slight mx-1"
+                    href="https://www.instagram.com/__chaitanya_kumar/"
+                  >
+                    <i className="fab fa-instagram" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </MDBFooter>
+  );
+};
+
+export default Footer;
